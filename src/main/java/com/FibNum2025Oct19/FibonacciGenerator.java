1ff76e9357e24738aa22947a42679cb495637771
orhankalayci2 <orhan@pmpkoc.net>
2024-10-19 21:32:28 +0300

    recursiveMethod

--- src/main/java/com/FibNum2025Oct19/FibonacciGenerator.java
+++ src/main/java/com/FibNum2025Oct19/FibonacciGenerator.java
@@ -2,8 +2,13 @@ package com.FibNum2025Oct19;
 
 public class FibonacciGenerator {
     public int getFibNum(int i) {
+        return recursiveMethod(i) ;
+    }
+
+    private int recursiveMethod (int i) {
         if (i ==0) return 0;
         if (i ==1) return 1;
         return getFibNum(i-1) + getFibNum(i-2);
     }
+
 }
