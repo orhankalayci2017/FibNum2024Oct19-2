acc1bfeedbb1d9d9b8ab8eb3a1505fb9c27a3ba6
orhankalayci2 <orhan@pmpkoc.net>
2024-10-19 20:56:29 +0300

    first tests

---
+++ src/main/java/com/FibNum2025Oct19/FibonacciGenerator.java
@@ -0,0 +1,8 @@
+package com.FibNum2025Oct19;
+
+public class FibonacciGenerator {
+    public int getFibNum(int i) {
+        if (i ==0) return 0;
+        return 1;
+    }
+}
---
+++ src/test/java/FibonacciGeneratorTest.java
@@ -0,0 +1,23 @@
+import com.FibNum2025Oct19.FibonacciGenerator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FibonacciGeneratorTest {
+
+    private final FibonacciGenerator fibonacciGenerator = new FibonacciGenerator();
+
+    @Test
+    public void testFibonacciOfZero() {
+        assertEquals(0, fibonacciGenerator.getFibNum(0), "Fibonacci of 0 should be 0");
+    }
+
+    @Test
+    public void testFibonacciOfOne() {
+        assertEquals(1, fibonacciGenerator.getFibNum(1), "Fibonacci of 1 should be 1");
+    }
+
+    @Test
+    public void testFibonacciOfTwo() {
+        assertEquals(1, fibonacciGenerator.getFibNum(2), "Fibonacci of 2 should be 1");
+    }
+}
